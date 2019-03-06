d68688a455bb73ca22bf737de741481b1764fe48
taner diler <taner.diler@gmail.com>
2019-03-06 22:54:23 +0300

    refactor build.bat

--- build.bat
+++ build.bat
@@ -1,22 +1,46 @@
-@echo off
-NC='\033[0m' # No Color
+echo "taner diler"
 
-logging=('logger-service' 'logging' 'named')
-feedlogger=('feed-logger' 'logging' 'named')
-consolelogger=('console-logger' 'logging' 'named')
-jeventbus=('jeventbus' '.' 'named')
-shared=('ecommerce-shared' 'ecommerce-platform' 'named')
-basket=('basket-service' 'ecommerce-platform' 'named')
-stock=('stock-service' 'ecommerce-platform' 'named')
-catalog=('catalog-service' 'ecommerce-platform' 'named')
-order=('order-service' 'ecommerce-platform' 'named')
-api=('ecommerce-api' 'ecommerce-platform' 'named')
+set JAVAHOME= "C:\Program Files\Java\jdk-11.0.2"
 
-modules=(logging feedlogger consolelogger jeventbus shared basket stock catalog order api)
+SETLOCAL
+CALL :Display logger-service , logging , named
+CALL :Display console-logger , logging , named
+CALL :Display feed-logger , logging , named
+CALL :Display jeventbus , . , named
+CALL :Display ecommerce-shared , ecommerce-platform , named
+CALL :Display basket-service , ecommerce-platform , named
+CALL :Display stock-service , ecommerce-platform , named
+CALL :Display catalog-service , ecommerce-platform , named
+CALL :Display order-service , ecommerce-platform , named
+CALL :Display ecommerce-api , ecommerce-platform , named
+EXIT /B %ERRORLEVEL%
 
 set modules[0].module = logger-service
 set modules[0].application = logging
-set modules[0].moduleType = named 
+set modules[0].moduleType = named
+
+:Display
+set moduleName=%~1
+set project=%~2
+set moduleType=%~3
+mkdir modules
+mkdir "out/%moduleName%"
+
+if "%moduleType%"=="named" (
+   dir /s /B %project%\%moduleName%\src\main\java\*.java > out\sources.txt
+	%JAVAHOME%\bin\javac --module-path modules -cp "lib\*" -d "out\%moduleName%" @out\sources.txt 
+) else (
+   dir /s /B %project%\%moduleName%\src\main\java\*.java | findstr /v /i "\module-info.java$" > out\sources.txt
+	%JAVAHOME%\bin\javac --module-path modules  -cp "lib\*" -d "out\%moduleName%" @out\sources.txt
+)
+cd "out/%moduleName%" && %JAVAHOME%\bin\jar --create --file "../../lib/%moduleName%.jar" -c . && cd ..
+cd ..
+echo The value of parameter 1 is %~1
+echo The value of parameter 2 is %~2
+if not "%moduleType%"=="unnamed" (
+   move "lib\%moduleName%.jar" modules\.
+)
+EXIT /B 0
 
 set modules[1].module = feed-service
 set modules[1].application = logging
@@ -55,17 +79,21 @@ set modules[4].application = ecommerce-platform
 set modules[4].moduleType = named
 
 
+
+set len=4
 set i=0
 :loop
 if %i% equ %len% goto :eof
-
+set modules[1]
 set cur.module=
 set cur.application=
 set cur.moduleType=
-for /f "usebackq delims==. tokens=1-3" %%j in (`set obj[%i%]`) do (
-	set cur.%%k=%%l
+for /f "usebackq delims==  tokens=1-3" %%j in (`set modules[%i%]`) do (
+	set cur.%%k=%%j
 )
 
+echo "taner diler"
+set cur.module
 mkdir out/%%cur.module%%
 if "%cur.moduleType%"=="named" (
 	cd out && javac --module-path ../modules -cp "../lib/*" -d $moduleName $(find ../$2/$1/src/main/java -name '*.java') && cd ..
