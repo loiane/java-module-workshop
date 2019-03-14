2a358011f59ea7b115fed539cbae68b82ac29cb8
Taner Diler <taner.diler@gmail.com>
2019-03-14 10:15:24 +0300

    Update build.sh to run automatic modules

--- build.sh
+++ build.sh
@@ -5,16 +5,16 @@ YELLOW='\033[1;33m'
 PURPLE='\033[1;35m'
 NC='\033[0m' # No Color
 
-logging=('logger-service' 'logging' 'named')
-feedlogger=('feed-logger' 'logging' 'named')
-consolelogger=('console-logger' 'logging' 'named')
-jeventbus=('jeventbus' '.' 'named')
-shared=('ecommerce-shared' 'ecommerce-platform' 'named')
-basket=('basket-service' 'ecommerce-platform' 'named')
-stock=('stock-service' 'ecommerce-platform' 'named')
-catalog=('catalog-service' 'ecommerce-platform' 'named')
-order=('order-service' 'ecommerce-platform' 'named')
-api=('ecommerce-api' 'ecommerce-platform' 'named')
+logging=('logger-service' 'logging' 'automatic' '')
+feedlogger=('feed-logger' 'logging' 'automatic' 'logger.service')
+consolelogger=('console-logger' 'logging' 'automatic' 'logger.service')
+jeventbus=('jeventbus' '.' 'named' '')
+shared=('ecommerce-shared' 'ecommerce-platform' 'automatic' 'jeventbus')
+basket=('basket-service' 'ecommerce-platform' 'automatic' 'jeventbus,ecommerce.shared')
+stock=('stock-service' 'ecommerce-platform' 'automatic' 'jeventbus,ecommerce.shared')
+catalog=('catalog-service' 'ecommerce-platform' 'automatic' 'jeventbus,ecommerce.shared')
+order=('order-service' 'ecommerce-platform' 'automatic' 'jeventbus,ecommerce.shared' )
+api=('ecommerce-api' 'ecommerce-platform' 'automatic' 'jeventbus,logger.service,feed.logger,console.logger,ecommerce.shared,basket.service,stock.service,catalog.service,order.service')
 
 modules=(logging feedlogger consolelogger jeventbus shared basket stock catalog order api)
 
@@ -29,21 +29,32 @@ recreateFolder() {
 
 recreateFolder "out"
 recreateFolder "lib"
-#recreateFolder "modules"
+recreateFolder "modules"
 
 compile() {
     moduleName="$1"
     projectName="$2"
     apiModuleType="$3"
+    modulesToAdd="$4"
 
     mkdir -p out/$moduleName
 
+    addModules=""
+    if [ ! -z "$modulesToAdd" ]; then
+       addModules="--add-modules $modulesToAdd"
+    fi
+
     if [ $apiModuleType = "named" ]; then
-       cd out && javac --module-path ../modules -cp "../lib/*" -d $moduleName $(find ../$2/$1/src/main/java -name '*.java') && cd ..
+       cd out && javac --module-path ../modules -cp "../lib/*"  -d $moduleName $(find ../$2/$1/src/main/java -name '*.java') && cd ..
     else
-       cd out && javac --module-path ../modules -cp "../lib/*" -d $moduleName $(find ../$2/$1/src/main/java -name '*.java' -not -name 'module-info.java') && cd ..
+       cd out && javac --module-path ../modules -cp "../lib/*" $addModules -d $moduleName $(find ../$2/$1/src/main/java -name '*.java' -not -name 'module-info.java')
+       cd ..
     fi    
     
+    if [ -d $2/$1/src/main/resource ]; then
+	cp -rf $2/$1/src/main/resource/* out/$moduleName/.
+    fi
+
     echo -e "${YELLOW}COMPILE ::${NC} Module $moduleName@$projectName compiled to ${GREEN}out/$moduleName${NC}"
 }
 
@@ -75,9 +86,10 @@ while [ $cnt -lt $total_Array ] ; do
         ((cnt=cnt+1))
 done
 
-if [ $apiModuleType = "named" -o $apiModuleType = "automatic" ]; then
-   echo execution module
-   java --module-path modules -cp "lib/*" --module ecommerce.api/ecommerce.api.Application
+if [ $apiModuleType = "named" ]; then
+   java --module-path modules  -cp "lib/*" --module ecommerce.api/ecommerce.api.Application
+elif [ $apiModuleType = "automatic" ]; then
+   java --module-path modules --add-modules ${api[3]} -cp "lib/*" --module ecommerce.api/ecommerce.api.Application
 else
    java --module-path modules -cp "lib/*" ecommerce.api.Application   
 fi
